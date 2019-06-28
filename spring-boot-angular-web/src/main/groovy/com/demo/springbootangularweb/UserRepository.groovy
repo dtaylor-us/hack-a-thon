4c74f9449e3fc453a490c4a925d3c879da701885
Derek <derektaylor.us@gmail.com>
2019-06-28 05:17:11 -0500

    add spring boot starter

--- spring-boot-angular-web/src/main/groovy/com/demo/springbootangularweb/UserRepository.groovy
+++
@@ -1,7 +0,0 @@
-package com.demo.springbootangularweb
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository extends CrudRepository<User, Long> {}
